bb42e6b369e92f74ad72c6a2884f06f022151d5e
Анна Терентьева <annyshka20072002@gmail.com>
2021-07-21 02:26:32 +0300

    Add scaling

--- js/hand.js
+++ js/hand.js
@@ -5,35 +5,36 @@ var wasPressed;
 
 hand.onclick = function () {
   wasPressed = "hand";
-};
+  panel.style.cursor = "grab";
 
-panel.onmousedown = function (current) {
-  if (wasPressed == "hand") {
-    panel.style.cursor = "grabbing";
-    let coords = getCoords(panel);
-    let scrollcoords = getCoords(scrollPanel);
-    let shiftX = current.pageX - coords.left + scrollcoords.left;
-    let shiftY = current.pageY - coords.top + scrollcoords.top;
+  panel.onmousedown = function (current) {
+    if (wasPressed == "hand") {
+      panel.style.cursor = "grabbing";
+      let coords = getCoords(panel);
+      let scrollcoords = getCoords(scrollPanel);
+      let shiftX = current.pageX - coords.left + scrollcoords.left;
+      let shiftY = current.pageY - coords.top + scrollcoords.top;
 
-    panel.onmousemove = function (current) {
-      panel.style.transform = "translate(0, 0)";
-      panel.style.top = current.pageY - shiftY + "px";
-      panel.style.left = current.pageX - shiftX + "px";
-    };
-
-    document.onmouseup = function () {
-      panel.style.cursor = "default";
-      panel.onmousemove = null;
-      document.onmouseup = null;
-    };
-
-    function getCoords(elem) {
-      let box = elem.getBoundingClientRect();
-      
-      return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset,
+      panel.onmousemove = function (current) {
+        panel.style.transform = "translate(0, 0)";
+        panel.style.top = current.pageY - shiftY + "px";
+        panel.style.left = current.pageX - shiftX + "px";
       };
+
+      document.onmouseup = function () {
+        panel.onmousemove = null;
+        document.onmouseup = null;
+        panel.style.cursor = "grab";
+      };
+
+      function getCoords(elem) {
+        let box = elem.getBoundingClientRect();
+
+        return {
+          top: box.top + pageYOffset,
+          left: box.left + pageXOffset,
+        };
+      }
     }
-  }
+  };
 };
